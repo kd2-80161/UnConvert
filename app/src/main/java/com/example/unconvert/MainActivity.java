7cd4133172245f061467da939db814278860daba
gs1719 <gauravsriup@gmail.com>
2021-09-07 21:33:54 +0530

    Fix app crashes on clicking submit button

--- app/src/main/java/com/example/unconvert/MainActivity.java
+++ app/src/main/java/com/example/unconvert/MainActivity.java
@@ -10,24 +10,30 @@ import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
     private TextView textView;
     private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button);
+        Button button = findViewById(R.id.button);
         textView = findViewById(R.id.textView);
         editText = findViewById(R.id.editText);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
             {
-//              Toast.makeText(MainActivity.this, "Hi click listener worked!", Toast.LENGTH_SHORT).show();
-                int kg  = Integer.parseInt(editText.getText().toString());
-                double pound = 2.205 * kg;
-                textView.setText("The corresponding value in Pounds is "+ pound);
+                try {
+                    String s = editText.getText().toString();
+              //Toast.makeText(MainActivity.this, "Hi click listener worked!", Toast.LENGTH_SHORT).show();
+                    int kg = Integer.parseInt(s);
+                    double pound = 2.205 * kg;
+                    textView.setText("The corresponding value in Pounds is " + pound);
+                }
+                catch (Exception e)
+                {
+                    Toast.makeText(MainActivity.this, "Empty", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
